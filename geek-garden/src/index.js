9af8210fc4b64dfd801cc6246eb0f9329c6f2bd3
fan-sir666 <fan_sir666@163.com>
2022-05-03 17:11:59 +0800

    添加:极客园后台完结

--- geek-garden/src/index.js
+++ geek-garden/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'normalize.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store'
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import { ConfigProvider } from 'antd';
+moment.locale('zh-cn');
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <ConfigProvider locale={zhCN}>
+                <App />
+            </ConfigProvider>
+        </PersistGate>
+    </Provider>);
